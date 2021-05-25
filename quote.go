c5ecc3a7150ce7ba5447031bc7477d0559e4b124
Tanwarat-Nat <tanwarat.nat@gmail.com>
2021-05-25 16:30:03 +0700

    release v1.0.0

--- quote.go
+++ quote.go
@@ -1,13 +1,16 @@
 package quote
 
+//Say : say hello
 func Say() string {
 	return "Hello"
 }
 
+//Speak : say hi mate
 func Speak() string {
 	return "Hi, mate."
 }
 
+//Talk : talk to Gohper!
 func Talk() string {
 	return "Gopher!"
 }
